6a8a504b66f579008911f24ab7a36098c6e6ad68
Güngör TURAN <turangungor1@gmail.com>
2022-10-10 10:59:42 +0300

    Final version

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Simulation testDrive = new Simulation();
+
+        ArrayList<Item> phase1 = testDrive.loadItems("phase-1.txt");
+        ArrayList<Item> phase2 = testDrive.loadItems("phase-2.txt");
+
+        ArrayList<Rocket> phase1_U1Rockets = testDrive.loadU1(phase1);
+        ArrayList<Rocket> phase2_U1Rockets = testDrive.loadU1(phase2);
+
+        System.out.println("Requiered total budget for U1 Rockets: " + (testDrive.runSimulation(phase1_U1Rockets) + testDrive.runSimulation(phase2_U1Rockets)) + "Millions $");
+
+        ArrayList<Rocket> phase1_U2Rockets = testDrive.loadU2(phase1);
+        ArrayList<Rocket> phase2_U2Rockets = testDrive.loadU2(phase2);
+
+        System.out.println("Requiered total budget for U2 Rockets: " + (testDrive.runSimulation(phase1_U2Rockets) + testDrive.runSimulation(phase2_U2Rockets)) + "Millions $");
     }
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -10,7 +10,7 @@ public class Simulation {
     public ArrayList<Item> loadItems(String fileName) throws FileNotFoundException {
         File file = new File(fileName);
         Scanner fileScanner = new Scanner(file);
-        ArrayList<Item> items = new ArrayList<Item>();
+        ArrayList<Item> items = new ArrayList<>();
 
         while (fileScanner.hasNextLine()){
             String line = fileScanner.nextLine();
@@ -19,12 +19,49 @@ public class Simulation {
         }
         return items;
     }
-    public ArrayList<U1> loadU1(ArrayList<Item> items){
-        ArrayList<U1> u1Ships = new ArrayList<U1>();
+    public ArrayList<Rocket> loadU1(ArrayList<Item> items){
+        ArrayList<Rocket> u1Ships = new ArrayList<>();
         Rocket rocket = new U1();
 
         for(Item i : items){
-
+            if (rocket.canCarry(i)){
+                rocket.carry(i);
+            }
+            else{
+                u1Ships.add(rocket);
+                rocket = new Rocket();
+            }
         }
+        return u1Ships;
+    }
+    public ArrayList<Rocket> loadU2(ArrayList<Item> items){
+        ArrayList<Rocket> u2Ships = new ArrayList<>();
+        Rocket rocket = new U2();
+
+        for(Item i : items){
+            if (rocket.canCarry(i)){
+                rocket.carry(i);
+            }
+            else{
+                u2Ships.add(rocket);
+                rocket = new Rocket();
+            }
+        }
+        return u2Ships;
+    }
+    public int runSimulation(ArrayList<Rocket> rocket){
+        //This part will calculate how much money we should pay for rockets.
+        int totalBudget = 0;
+        for(Rocket i : rocket){
+            while(!i.launch()){
+                totalBudget += i.cost;
+            }
+            totalBudget += i.cost;
+            while(!i.land()){
+                totalBudget +=i.cost;
+            }
+            totalBudget += i.cost;
+        }
+        return totalBudget;
     }
 }
